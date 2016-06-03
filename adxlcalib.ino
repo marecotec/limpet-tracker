127bb6c33f79701f65583472c0f852d0a4a14041
osu01c <osu01c@bangor.ac.uk>
2016-06-03 13:22:09 +0100

    Add files via upload

---
+++ adxlcalib.ino
@@ -0,0 +1,57 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+ 
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+ 
+ 
+float AccelMinX = 0;
+float AccelMaxX = 0;
+float AccelMinY = 0;
+float AccelMaxY = 0;
+float AccelMinZ = 0;
+float AccelMaxZ = 0;
+ 
+ 
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("ADXL345 Accelerometer Calibration"); 
+  Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+}
+ 
+void loop(void)
+{
+    Serial.println("Type key when ready..."); 
+    while (!Serial.available()){}  // wait for a character
+    
+    /* Get a new sensor event */ 
+    sensors_event_t accelEvent;  
+    accel.getEvent(&accelEvent);
+    
+    if (accelEvent.acceleration.x < AccelMinX) AccelMinX = accelEvent.acceleration.x;
+    if (accelEvent.acceleration.x > AccelMaxX) AccelMaxX = accelEvent.acceleration.x;
+    
+    if (accelEvent.acceleration.y < AccelMinY) AccelMinY = accelEvent.acceleration.y;
+    if (accelEvent.acceleration.y > AccelMaxY) AccelMaxY = accelEvent.acceleration.y;
+  
+    if (accelEvent.acceleration.z < AccelMinZ) AccelMinZ = accelEvent.acceleration.z;
+    if (accelEvent.acceleration.z > AccelMaxZ) AccelMaxZ = accelEvent.acceleration.z;
+  
+    Serial.print("Accel Minimums: "); Serial.print(AccelMinX); Serial.print("  ");Serial.print(AccelMinY); Serial.print("  "); Serial.print(AccelMinZ); Serial.println();
+    Serial.print("Accel Maximums: "); Serial.print(AccelMaxX); Serial.print("  ");Serial.print(AccelMaxY); Serial.print("  "); Serial.print(AccelMaxZ); Serial.println();
+ 
+    while (Serial.available())
+    {
+      Serial.read();  // clear the input buffer
+    }
+}
